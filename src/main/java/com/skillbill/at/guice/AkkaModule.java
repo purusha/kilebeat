8641f6e1484279ddfa81fc0fff67e22159496d0b
alan toro <alanprog@gmail.com>
2017-07-26 21:57:59 +0200

    file system watcher MUST be singleton

--- src/main/java/com/skillbill/at/guice/AkkaModule.java
+++ src/main/java/com/skillbill/at/guice/AkkaModule.java
@@ -4,9 +4,11 @@ import java.io.File;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
+import com.google.inject.Singleton;
 import com.skillbill.at.configuration.ConfigurationValidator;
 import com.skillbill.at.configuration.ConfigurationValidator.ExportsConfiguration;
 import com.skillbill.at.configuration.ConfigurationValidator.ValidationResponse;
+import com.skillbill.at.service.FileSystemWatcher;
 
 public class AkkaModule implements Module {
 	@Override
@@ -17,6 +19,11 @@ public class AkkaModule implements Module {
 		if (!validResp.isValid()) {
 			throw new RuntimeException("config.file is INVALID");
 		}
+		
+		binder
+			.bind(FileSystemWatcher.class)
+			.to(FileSystemWatcher.class)
+			.in(Singleton.class);
 		
 		binder
 			.bind(ExportsConfiguration.class)
